9749dbd81a1d713551a3ce21e6a545f310cd17d6
artkwi <artkwi@devanddeliver.com>
2020-06-10 14:01:16 +0200

    feat: .gitignore - /build removed

---
+++ build/precache-manifest.bc35b215b2af6d3d4706bf3ab0aacbe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea7f9c4d3b8b246002f697953685a79",
+    "url": "/artkwi.github.io/index.html"
+  },
+  {
+    "revision": "20e416dabdb2c5785582",
+    "url": "/artkwi.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d956070bd840e50e761",
+    "url": "/artkwi.github.io/static/js/2.de9b437f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/artkwi.github.io/static/js/2.de9b437f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e416dabdb2c5785582",
+    "url": "/artkwi.github.io/static/js/main.fbae91af.chunk.js"
+  },
+  {
+    "revision": "ca49bd9b62cc8794fcd3",
+    "url": "/artkwi.github.io/static/js/runtime-main.8fc0a379.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artkwi.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
