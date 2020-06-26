4049434e447e823f9dbc2c7654b1fb82a2cadf78
artkwi <artkwi@devanddeliver.com>
2020-06-26 12:34:39 +0200

    feat: mobile burger

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Styled from './styled'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav>
 
       <Styled.Items>
+        <Styled.MenuBtn isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} role="button"><Styled.Lines isOpen={isOpen}/></Styled.MenuBtn>
         <Styled.Item title="o mnie">O mnie</Styled.Item>
         <Styled.Item title="wykształcenie">Wykształcenie</Styled.Item>
         <Styled.Item title="Doświadczenie">Doświadczenie</Styled.Item>
--- src/components/Navbar/styled.ts
+++ src/components/Navbar/styled.ts
@@ -1,4 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const lineCSS = css`
+    background-color: #fff;
+    border-radius: 1px;
+    height: 2px;
+    width: 100%;
+    transition: all 0.2s;
+`;
 
 export const Items = styled.ul`
     display: grid;
@@ -9,4 +17,36 @@ export const Item = styled.li`
     background: yellow;
     padding: 20px;
     text-align: center;
+`;
+
+export const MenuBtn = styled.a<{ isOpen: boolean }>`
+    display: grid;
+    grid-template-columns: 1fr;
+    align-items: center;
+    height: 30px;
+    width: 30px;
+    background: red;
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    z-index: 10;
+`;
+
+export const Lines = styled.span<{ isOpen: boolean }>`
+    ${lineCSS};
+    height: ${({ isOpen }) => (isOpen ? '0' : '2px')};
+    &:after,
+    &:before {
+        content: '';
+        ${lineCSS};
+        background-color: ${({ isOpen }) => (isOpen ? '#000' : '#FFF')};
+        position: absolute;
+        transform: translate(0, 8px);
+        transform: ${({ isOpen }) => isOpen && 'rotate(-45deg)'};
+    }
+
+    &:before {
+        transform: translate(0, -8px);
+        transform: ${({ isOpen }) => isOpen && 'rotate(45deg)'};
+    }
 `;
